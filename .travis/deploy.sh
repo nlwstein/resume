5b40b69e6d366af5a33bb7b620f8d4283c198348
Nick Stein <nick@trellis.co>
2018-05-25 22:24:48 -0400

    Switched to generic language declaration

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,5 +4,5 @@ chmod 600 .travis/id_rsa # Allow read access to the private key
 ssh-add .travis/id_rsa # Add the private key to SSH
 
 git config --global push.default matching
-# git remote add deploy ssh://nstein@stein.boston:22/var/www/html
+git remote add deploy ssh://nstein@stein.boston:22/var/www/html
 git push deploy master
